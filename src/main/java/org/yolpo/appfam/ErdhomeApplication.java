42747ca4abf1039792d3ef7c315c34fef8f41a29
Yolepo <marcjalin@gmail.com>
2017-04-09 18:25:58 +0200

    Initial commit

---
+++ src/main/java/org/yolpo/appfam/ErdhomeApplication.java
@@ -0,0 +1,12 @@
+package org.yolpo.appfam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdhomeApplication.class, args);
+	}
+}
---
+++ src/test/java/org/yolpo/appfam/ErdhomeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yolpo.appfam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErdhomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
